c54d30d5255abd2fd8f7ccf84046a28e8165b3b6
Aayush-Pandey05 <aayushpandeydsce23@gmail.com>
2025-11-22 11:05:30 +0530

    Feature: ci pipeline

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log("Hello world");
+console.log("Hello i am learning ci/cd using github actions");
---
+++ tests.js
@@ -0,0 +1,5 @@
+console.log("running tests");
+setTimeout(() => {
+    console.log("Tests running for 3 seconds")
+}, 3000);
+console.log("tests completed");
